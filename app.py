ecd4a4879ee78da0d19b890211b5f1675128db6c
CodeCloutDev <71075659+CodeCloutDevs@users.noreply.github.com>
2022-09-29 10:47:26 +0200

    added headings

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ tickerSymbol = "GOOGL"
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period="1d", start="2010-5-31", end="2020-5-31")
 
+st.write(
+    """
+    ## Close Price
+    """
+)
 st.line_chart(tickerDf.Close)
+st.write(
+    """
+    ## Volume Price
+    """
+)
 st.line_chart(tickerDf.Volume)
